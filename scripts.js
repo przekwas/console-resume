d7b3597638dd3a662e8ea02f395a884df477e5c8
Luke Przekwas <galactica@Lukes-MacBook-Pro.local>
2018-01-10 14:55:39 -0600

    name requirement

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("Hello World!");
+let name = "Luke Przekwas";
+
+//logs top decorative line plus my name using js to capitalize it
+console.log("```````````````````````");
+console.log("Name: " + name.toUpperCase());
+
+
+
