bbf066cf30ac22cda5064f34ac5eaedd6a729d09
Luke Przekwas <galactica@Lukes-MacBook-Pro.local>
2018-01-10 15:12:11 -0600

    work experience and interests

--- scripts.js
+++ scripts.js
@@ -1,8 +1,26 @@
 let name = "Luke Przekwas";
 
 //logs top decorative line plus my name using js to capitalize it
-console.log("```````````````````````");
+console.log("````````````````````````");
 console.log("Name: " + name.toUpperCase());
-
+console.log("Career: Covalence Student / Barista");
+console.log("Description: How would I describe myself? Three words. Hard-working, Alpha male. Jackhammer. Merciless. Insatiable.");
+console.log("\n");
+//starts logs for my interests
+console.log("My Interests:");
+console.log("* Super Smash Bros Tournaments");
+console.log("* Learning how to code!");
+console.log("* Coffee");
+console.log("* Soccer");
+console.log("* Metal Guitar");
+console.log("\n");
+//starts logs for my experience
+console.log("My Previous Experience:");
+console.log("* FOH Manager at Urban Standard, 8 years");
+console.log("- Ran operations, did bookkeeping, inventory management, interviewing, and customer service relations.");
+console.log("* Physics Tutor at Samford University, 2 years");
+console.log("- Tutored Pre-Med students in physcis after school hours");
+console.log("* Pathology Lab Intern, 1 year");
+console.log("- Studied effects of beta-estrogen on repurfusion injuries, assisted lab doctors, took notes")
 
 
